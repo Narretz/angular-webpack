a5c497a9acf32ed86ee04b259ff57fa11cafe621
Martin Staffa <mjstaffa@googlemail.com>
2016-02-21 20:44:38 +0100

    use Chrome instead of PhantomJS

--- karma.conf.js
+++ karma.conf.js
@@ -30,8 +30,8 @@ module.exports = function karmaConfig (config) {
     },
 
     browsers: [
-      // Run tests using PhantomJS2
-      'PhantomJS2'
+      // Run tests using Chrome
+      'Chrome'
     ],
 
     singleRun: true,
